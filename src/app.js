0178dafb2e1fd56263766e97c6254d86a85bdbf2
suizoperuano <andrechedel@hotmail.com>
2023-01-29 19:16:56 +0000

    js

---
+++ src/app.js
@@ -0,0 +1,26 @@
+let who = [
+  "My parents",
+  "My wife",
+  "The racoon from the neigborhood",
+  "My dog",
+  "My cat",
+  "My boss"
+];
+let action = ["destroyed", "took away", "lost", "broke", "deleted"];
+let what = [
+  "my homework",
+  "my work computer",
+  "my pencil",
+  "the documentation",
+  "the project"
+];
+let when = [
+  "rigth before the class",
+  "just before",
+  "after i finished",
+  "when i was not looking",
+  "while i was watching watching youtube"
+];
+function generate() {
+  console.log("it works");
+}
